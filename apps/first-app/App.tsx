04e304131106a39d673f856c4eacb8f693ee5f12
Giau. Tran Minh <giau.tmg@gmail.com>
2021-02-11 02:18:16 +0700

    chore: reproduce expo-font bug

--- apps/first-app/App.tsx
+++ apps/first-app/App.tsx
@@ -1,13 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as ExpoFont from 'expo-font'
 import { StyleSheet, Text, View } from "react-native";
 import firstMessage from "first-package";
 import secondMessage from "second-package";
 
+import Share16 from './share16.svg'
+
 export default function App() {
+  const [ready, setReady] = useState(false)
+  useEffect(() => {
+    ;(async () => {
+      try {
+        await ExpoFont.loadAsync({
+          'Roboto-Black': require('./Roboto-Black.ttf'),
+        })
+        setReady(true)
+      } catch (e) {
+        console.log('e', e)
+      }
+    })();
+  }, [])
+
+  if (!ready) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello from the first application</Text>
+      <Text style={{
+        fontFamily: 'Roboto-Black',
+      }}>Hello from the first application</Text>
+      <Share16/>
       <Text>{firstMessage}</Text>
       <Text>{secondMessage}</Text>
       <StatusBar style="auto" />
--- apps/first-app/metro.config.js
+++ apps/first-app/metro.config.js
@@ -1,3 +1,24 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces');
+const { createMetroConfiguration } = require('expo-yarn-workspaces')
 
-module.exports = createMetroConfiguration(__dirname);
+const defaultSourceExts = ['ts', 'tsx', 'js', 'jsx', 'json', 'wasm']
+
+module.exports = (() => {
+  const cfg = createMetroConfiguration(__dirname)
+
+  const {
+    resolver: { sourceExts = defaultSourceExts, assetExts },
+  } = cfg
+
+  return {
+    ...cfg,
+    transformer: {
+      ...cfg.transformer,
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      ...cfg.resolver,
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  }
+})()
